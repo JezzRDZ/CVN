232e72ddb8676f3d07e765f10ac84b1734d1d114
Jez <jez.rdz95@gmail.com>
2019-05-31 17:58:06 -0500

    headers and frontend

--- page/static/js/main.js
+++ page/static/js/main.js
@@ -1,23 +1,35 @@
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+// Closes responsive menu when a scroll trigger link is clicked
+$('.js-scroll-trigger').click(function() {
+  $('.navbar-collapse').collapse('hide');
+});
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 57
-  });
+// Activate scrollspy to add active class to navbar items on scroll
+$('body').scrollspy({
+  target: '#mainNav',
+  offset: 57
+});
 
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+// Collapse Navbar
+var navbarCollapse = function() {
+  if ($("#mainNav").offset().top > 100) {
+    $("#mainNav").addClass("navbar-shrink");
+  } else {
+    $("#mainNav").removeClass("navbar-shrink");
+  }
+};
+// Collapse now if page is not at top
+navbarCollapse();
+// Collapse the navbar when page is scrolled
+$(window).scroll(navbarCollapse);
+
+//Parallax on header
+/*$(document).scroll(function() {
+  var st = $(this).scrollTop();
+  $("#header").css({
+  "background-position-y": (-st/20)
+  })
+  $("#headerc").css({
+  "top": (-st/5),
+  "bottom": (st/5)
+  })
+});*/
--- page/views.py
+++ page/views.py
@@ -1,6 +1,4 @@
-from django.core.mail import EmailMessage
-from django.shortcuts import render, redirect
-from django.template.loader import get_template
+from django.shortcuts import render
 from django.utils import timezone
 from .models import Event
 from .forms import ContactForm
@@ -16,28 +14,4 @@ def home(request):
 
 def contact(request):
 	form_class = ContactForm
-	if request.method == 'POST':
-		form = form_class(data = request.POST)
-		if form.is_valid():
-			name = request.POST.get('name', '')
-			phone = request.POST.get('phone', '')
-			mail = request.POST.get('mail', '')
-			content = request.POST.get('content', '')
-			template = get_template('cvn/contact_template.txt')
-			context = {
-				'name': name,
-				'phone': phone,
-				'mail': mail,
-				'content': content,
-			}
-			content = template.render(context)
-			mail = EmailMessage(
-				"Nueva solicitud de contacto",
-				content,
-				"Your website" +'',
-				['jez.rdz95@gmail.com'],
-				headers = {'Reply to': mail}
-			)
-			email.send()
-			return redirect('contact')
 	return render(request, 'cvn/contact.html', {'form': form_class})
