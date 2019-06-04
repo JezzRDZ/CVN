97769096e41fe49e7b2cc96e329ee484185414b6
Jez <jez.rdz95@gmail.com>
2019-06-04 10:19:30 -0500

    gmail smtp configured

--- cvnpage/settings.py
+++ cvnpage/settings.py
@@ -121,4 +121,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Email settings
+# Using Gmal SMTP
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'webpage.cvn@gmail.com'
+EMAIL_HOST_PASSWORD = 'webcvn123$'
--- page/views.py
+++ page/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.utils import timezone
@@ -6,13 +7,18 @@ from .forms import ContactForm
 from django.core.mail import send_mail, BadHeaderError
 from django.template.loader import get_template
 
+# Página index
+def home(request):
+	return render(request, 'cvn/index.html', {})
+
+# Página Eventos
+# Publica los que tienen cumplen con la fecha de publicación y se ordenan por fecha de publicación.
 def events(request):
 	events = Event.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
 	return render(request, 'cvn/events.html', {'events': events})
 
-def home(request):
-	return render(request, 'cvn/index.html', {})
-
+# Página Contacto
+# Recoge datos del form y envía un email al correo de contacto.
 def contact(request):
 	if request.method == 'GET':
 		form_class = ContactForm()
@@ -32,11 +38,13 @@ def contact(request):
 			}
 			form_content = template.render(context)
 			try:
-				send_mail(name, form_content, mail, ['contacto@comunidadvidanueva.com'])
+				send_mail(name, form_content, settings.EMAIL_HOST_USER, ['jez.rdz95@gmail.com'])
 			except BadHeaderError:
 				return HttpResponse('Invalid header found.')
 			return redirect('success')
 	return render(request, 'cvn/contact.html', {'form': form_class})
 
+# Página Success
+# Se carga cuando el correo de contacto es mandado satisfactoriamente.
 def success(request):
 	return render(request, 'cvn/email_send.html', {})
