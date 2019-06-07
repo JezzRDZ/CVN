d18134d01b9109cac3bca275b30c9cb1a858d9c3
Jez <jez.rdz95@gmail.com>
2019-06-07 13:45:46 -0500

    responsive index content

--- page/static/js/main.js
+++ page/static/js/main.js
@@ -1,20 +1,3 @@
-(function($) {
-  "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 57)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
-
 // Closes responsive menu when a scroll trigger link is clicked
 $('.js-scroll-trigger').click(function() {
   $('.navbar-collapse').collapse('hide');
@@ -50,25 +33,3 @@ $(window).scroll(navbarCollapse);
   "bottom": (st/5)
   })
 });*/
-
-// Closes responsive menu when a scroll trigger link is clicked
-$('.js-scroll-trigger').click(function() {
-  $('.navbar-collapse').collapse('hide');
-});
-
-// Scroll reveal calls
-window.sr = ScrollReveal();
-sr.reveal('.sr-icons', {
-  duration: 600,
-  scale: 0.3,
-  distance: '0px'
-}, 200);
-sr.reveal('.sr-button', {
-  duration: 1000,
-  delay: 200
-});
-sr.reveal('.sr-contact', {
-  duration: 600,
-  scale: 0.3,
-  distance: '0px'
-}, 300);
